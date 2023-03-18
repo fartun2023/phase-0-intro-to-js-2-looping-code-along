c3744e4c18e6155f763de5db0d6d4e879d0771ca
fartun <fartunwario34@gmail.com>
2023-03-18 17:42:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(namesArray, event) {
+    const messagesArray = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      const message = `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`;
+      messagesArray.push(message);
+    }
+    return messagesArray;
+  }
+  function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
